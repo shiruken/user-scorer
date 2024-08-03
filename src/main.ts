00184ba8bc5dbddf3d586b488b860ecb33eeb513
Colin Sullender <shiruken343@gmail.com>
2024-08-02 19:42:52 -0600

    Remove 'Get Settings' debugging menu action

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Devvit } from '@devvit/public-api';
 import { onCommentSubmit, showUserScore, onModAction } from './handlers.js';
-import { getAppSettings, settings } from './settings.js';
+import { settings } from './settings.js';
 
 Devvit.configure({
   redditAPI: true,
@@ -49,16 +49,6 @@ Devvit.addMenuItem({
     console.log(`\n#users: ${JSON.stringify(users, null, 2)}`);
     const user = await context.redis.hgetall("shiruken");
     console.log(`\nu/shiruken: ${JSON.stringify(user, null, 2)}`);
-  },
-});
-
-Devvit.addMenuItem({
-  label: 'Get Settings',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async(_event, context) => {
-    const settings = await getAppSettings(context.settings);
-    console.log(settings);
   },
 });
 
