5a78745b151d3314cc58e8233a3e0c894c7319aa
Colin Sullender <shiruken343@gmail.com>
2024-08-08 20:59:34 -0600

    Menu actions to add/remove test users

--- src/main.ts
+++ src/main.ts
@@ -57,5 +57,52 @@ Devvit.addSchedulerJob({
 //     console.log(`\nu/shiruken: ${JSON.stringify(user, null, 2)}`);
 //   },
 // });
+Devvit.addMenuItem({
+  label: 'Add Test Users',
+  location: 'subreddit',
+  forUserType: 'moderator',
+  onPress: async(_event, context) => {
+    await context.redis.zAdd("#users", { member: "A", score: -1 });
+    await context.redis.zAdd("#users", { member: "B", score: 0 });
+    await context.redis.zAdd("#users", { member: "C", score: 0.1 });
+    await context.redis.zAdd("#users", { member: "D", score: 0.2 });
+    await context.redis.zAdd("#users", { member: "E", score: 0.3 });
+    await context.redis.zAdd("#users", { member: "F", score: 0.4 });
+    await context.redis.zAdd("#users", { member: "G", score: 0.5 });
+    await context.redis.zAdd("#users", { member: "H", score: 0.6 });
+    await context.redis.zAdd("#users", { member: "I", score: 0.7 });
+    await context.redis.zAdd("#users", { member: "J", score: 0.8 });
+    await context.redis.zAdd("#users", { member: "K", score: 0.9 });
+    await context.redis.zAdd("#users", { member: "L", score: 1.0 });
+    await context.redis.zAdd("#users", { member: "M", score: 0.05 });
+    await context.redis.zAdd("#users", { member: "N", score: 0.15 });
+    await context.redis.zAdd("#users", { member: "O", score: 0.25 });
+    await context.redis.zAdd("#users", { member: "P", score: 0.35 });
+    await context.redis.zAdd("#users", { member: "Q", score: 0.45 });
+    await context.redis.zAdd("#users", { member: "R", score: 0.55 });
+    await context.redis.zAdd("#users", { member: "S", score: 0.65 });
+    await context.redis.zAdd("#users", { member: "T", score: 0.75 });
+    await context.redis.zAdd("#users", { member: "U", score: 0.85 });
+    await context.redis.zAdd("#users", { member: "V", score: 0.95 });
+    await context.redis.zAdd("#users", { member: "W", score: 0.01 });
+    await context.redis.zAdd("#users", { member: "X", score: 0.99 });
+    await context.redis.zAdd("#users", { member: "Y", score: 0 });
+    await context.redis.zAdd("#users", { member: "Z", score: 1.0 });
+    console.log("Added test users");
+  },
+});
+
+Devvit.addMenuItem({
+  label: 'Remove Test Users',
+  location: 'subreddit',
+  forUserType: 'moderator',
+  onPress: async(_event, context) => {
+    await context.redis.zRem("#users", [
+      "A","B","C","D","E","F","G","H","I","J","K","L","M",
+      "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    ]);
+    console.log("Removed test users");
+  },
+});
 
 export default Devvit;
