164122d54319a0da15d7761834647ded65796430
Colin Sullender <shiruken343@gmail.com>
2024-08-09 20:00:40 -0600

    Disable debug actions

--- src/main.ts
+++ src/main.ts
@@ -44,51 +44,55 @@ Devvit.addMenuItem({
   onPress: generateReport,
 });
 
-Devvit.addMenuItem({
-  label: 'Clear User Score for u/shiruken',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async(_event, context) => {
-    await context.redis.del("shiruken");
-    await context.redis.zRem("#users", ["shiruken"]);
-    console.log("Deleted u/shiruken from Redis");
-  },
-});
+// Devvit.addMenuItem({
+//   label: 'Clear User Score for u/shiruken',
+//   location: 'subreddit',
+//   forUserType: 'moderator',
+//   onPress: async(_event, context) => {
+//     await context.redis.del("shiruken");
+//     await context.redis.zRem("#users", ["shiruken"]);
+//     console.log("Deleted u/shiruken from Redis");
+//     context.ui.showToast("Deleted u/shiruken from Redis");
+//   },
+// });
 
-Devvit.addMenuItem({
-  label: 'Get Tracked Users',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async(_event, context) => {
-    const users = await context.redis.zRange("#users", 0, -1);
-    console.log(`\n#users: ${JSON.stringify(users, null, 2)}`);
-    const user = await context.redis.hgetall("shiruken");
-    console.log(`\nu/shiruken: ${JSON.stringify(user, null, 2)}`);
-  },
-});
+// Devvit.addMenuItem({
+//   label: 'Get Tracked Users',
+//   location: 'subreddit',
+//   forUserType: 'moderator',
+//   onPress: async(_event, context) => {
+//     const users = await context.redis.zRange("#users", 0, -1);
+//     console.log(`\n#users: ${JSON.stringify(users, null, 2)}`);
+//     const user = await context.redis.hgetall("shiruken");
+//     console.log(`\nu/shiruken: ${JSON.stringify(user, null, 2)}`);
+//     context.ui.showToast("See console output");
+//   },
+// });
 
-Devvit.addMenuItem({
-  label: 'Add Test Users',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async(_event, context) => {
-    for (let i = 0; i < 10000; i++) {
-      await context.redis.zAdd("#users", { member: `TestUser_${i}`, score: Math.random() });
-    }
-    console.log(`Added test users`);
-  },
-});
+// Devvit.addMenuItem({
+//   label: 'Add Test Users',
+//   location: 'subreddit',
+//   forUserType: 'moderator',
+//   onPress: async(_event, context) => {
+//     for (let i = 0; i < 10000; i++) {
+//       await context.redis.zAdd("#users", { member: `TestUser_${i}`, score: Math.random() });
+//     }
+//     console.log(`Added test users`);
+//     context.ui.showToast("Added test users");
+//   },
+// });
 
-Devvit.addMenuItem({
-  label: 'Remove Test Users',
-  location: 'subreddit',
-  forUserType: 'moderator',
-  onPress: async(_event, context) => {
-    for (let i = 0; i < 10000; i++) {
-      await context.redis.zRem("#users", [`TestUser_${i}`]);
-    }
-    console.log(`Removed test users`);
-  },
-});
+// Devvit.addMenuItem({
+//   label: 'Remove Test Users',
+//   location: 'subreddit',
+//   forUserType: 'moderator',
+//   onPress: async(_event, context) => {
+//     for (let i = 0; i < 10000; i++) {
+//       await context.redis.zRem("#users", [`TestUser_${i}`]);
+//     }
+//     console.log(`Removed test users`);
+//     context.ui.showToast("Removed test users");
+//   },
+// });
 
 export default Devvit;
